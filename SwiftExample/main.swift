fa24601b545cf3613d95fe7bb5fee60d4fcf0d5c
Marcelo Ludovico de Paula <marcelo.paula@eptv.com.br>
2020-12-04 17:59:56 -0300

    Initial commit

--- SwiftExample/main.swift
+++ SwiftExample/main.swift
@@ -8,5 +8,37 @@
 
 import Foundation
 
-print("Hello, World!")
+class Track{
+    let name: String
+    let instructor: String
+    
+    init(name: String, instructor: String) {
+        self.name = name
+        self.instructor = instructor
+    }
+}
 
+class Teacher{
+    
+}
+
+let tracks: [Track] = [
+    Track(name: "Mobile", instructor: "Tommy"),
+    Track(name: "Web", instructor: "Brian"),
+    Track(name: "Games", instructor: "Colton")
+]
+
+let students = [
+    "Harry", "Ron", "Hermione"
+]
+
+var assignments: [String : Track] = [:]
+
+for student in students{
+    let track = tracks[Int.random(in: 0 ..< tracks.count)]
+    assignments[student] = track
+}
+
+for (student, track) in assignments{
+    print("\(student) got \(track.name) with \(track.instructor)")
+}
