e4c669ae6ae491240e3e3ee9c8c6a9e29552b3d0
Marcelo Ludovico de Paula <marcelo.paula@eptv.com.br>
2020-12-04 17:44:08 -0300

    Initial Commit

---
+++ SwiftExample/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftExample
+//
+//  Created by marcelo on 04/12/2020.
+//  Copyright © 2020 EPTV. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
